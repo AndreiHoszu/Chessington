c6640650fdbb229bbd9e0950000781ebc5f55b1b
Andrei Hoszu <andrei.hoszu@softwire.com>
2022-06-30 09:05:31 +0100

    rooks can now move

--- Chessington.GameEngine/Pieces/Rook.cs
+++ Chessington.GameEngine/Pieces/Rook.cs
@@ -10,7 +10,26 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var moves = Enumerable.Empty<Square>();
+            var piece = board.FindPiece(this);
+
+            for (var i = 0; i < 8; i++)
+            {
+                if (piece.Row != i)
+                {
+                    moves = moves.Concat(new[] { new Square(i, piece.Col) });
+                }
+            }
+
+            for (var i = 0; i < 8; i++)
+            {
+                if (piece.Col != i)
+                {
+                    moves = moves.Concat(new[] { new Square(piece.Row, i) });
+                }
+            }
+
+            return moves;
         }
     }
 }
