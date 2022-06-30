7534eef9e15618dba6b4cb71ca102fe8ab39ea2a
Andrei Hoszu <andrei.hoszu@softwire.com>
2022-06-30 10:38:47 +0100

    knight can now attack

--- Chessington.GameEngine/Pieces/Knight.cs
+++ Chessington.GameEngine/Pieces/Knight.cs
@@ -13,44 +13,137 @@ namespace Chessington.GameEngine.Pieces
             var moves = Enumerable.Empty<Square>();
             var piece = board.FindPiece(this);
 
-            if (piece.Row + 1 < 8 && piece.Col + 2 < 8)
+            
+            if (piece.Row + 1 < 8)
             {
-                moves = moves.Concat(new[] { new Square(piece.Row + 1, piece.Col + 2) });
+                if (piece.Col + 2 < 8)
+                {
+                    if (board.GetPiece(new Square(piece.Row + 1, piece.Col + 2)) != null)
+                    {
+                        if (board.GetPiece(new Square(piece.Row + 1, piece.Col + 2)).Player != board.GetPiece(piece).Player)
+                        {
+                            moves = moves.Concat(new[] { new Square(piece.Row + 1, piece.Col + 2) });
+                        }
+                    }
+                    else
+                    {
+                        moves = moves.Concat(new[] { new Square(piece.Row + 1, piece.Col + 2) });
+                    }
+                }
+
+                if (piece.Col - 2 >= 0)
+                {
+                    if (board.GetPiece(new Square(piece.Row + 1, piece.Col - 2)) != null)
+                    {
+                        if (board.GetPiece(new Square(piece.Row + 1, piece.Col - 2)).Player != board.GetPiece(piece).Player)
+                        {
+                            moves = moves.Concat(new[] { new Square(piece.Row + 1, piece.Col - 2) });
+                        }
+                    }
+                    else
+                    {
+                        moves = moves.Concat(new[] { new Square(piece.Row + 1, piece.Col - 2) });
+                    }
+                }
             }
 
-            if (piece.Row + 1 < 8 && piece.Col - 2 >= 0)
+            if (piece.Row + 2 < 8)
             {
-                moves = moves.Concat(new[] { new Square(piece.Row + 1, piece.Col - 2) });
+                if( piece.Col + 1 < 8)
+                {
+                    if (board.GetPiece(new Square(piece.Row + 2, piece.Col + 1)) != null)
+                    {
+                        if (board.GetPiece(new Square(piece.Row + 2, piece.Col + 1)).Player != board.GetPiece(piece).Player)
+                        {
+                            moves = moves.Concat(new[] { new Square(piece.Row + 2, piece.Col + 1) });
+                        }
+                    }
+                    else
+                    {
+                        moves = moves.Concat(new[] { new Square(piece.Row + 2, piece.Col + 1) });
+                    }
+                }
+
+                if (piece.Col - 1 >= 0)
+                {
+                    if (board.GetPiece(new Square(piece.Row + 2, piece.Col - 1)) != null)
+                    {
+                        if (board.GetPiece(new Square(piece.Row + 2, piece.Col - 1)).Player != board.GetPiece(piece).Player)
+                        {
+                            moves = moves.Concat(new[] { new Square(piece.Row + 2, piece.Col - 1) });
+                        }
+                    }
+                    else
+                    {
+                        moves = moves.Concat(new[] { new Square(piece.Row + 2, piece.Col - 1) });
+                    }
+                }
             }
 
-            if (piece.Row + 2 < 8 && piece.Col + 1 < 8)
+            if (piece.Row - 1 >= 0)
             {
-                moves = moves.Concat(new[] { new Square(piece.Row + 2, piece.Col + 1) });
+                if (piece.Col + 2 < 8)
+                {
+                    if (board.GetPiece(new Square(piece.Row - 1, piece.Col + 2)) != null)
+                    {
+                        if (board.GetPiece(new Square(piece.Row - 1, piece.Col + 2)).Player != board.GetPiece(piece).Player)
+                        {
+                            moves = moves.Concat(new[] { new Square(piece.Row - 1, piece.Col + 2) });
+                        }
+                    }
+                    else
+                    {
+                        moves = moves.Concat(new[] { new Square(piece.Row - 1, piece.Col + 2) });
+                    }
+                }
+
+                if (piece.Col - 2 >= 0)
+                {
+                    if (board.GetPiece(new Square(piece.Row - 1, piece.Col - 2)) != null)
+                    {
+                        if (board.GetPiece(new Square(piece.Row - 1, piece.Col - 2)).Player != board.GetPiece(piece).Player)
+                        {
+                            moves = moves.Concat(new[] { new Square(piece.Row - 1, piece.Col - 2) });
+                        }
+                    }
+                    else
+                    {
+                        moves = moves.Concat(new[] { new Square(piece.Row - 1, piece.Col - 2) });
+                    }
+                }
             }
 
-            if (piece.Row + 2 < 8 && piece.Col -1 >= 0)
+            if (piece.Row - 2 >= 0)
             {
-                moves = moves.Concat(new[] { new Square(piece.Row + 2, piece.Col - 1) });
-            }
+                if (piece.Col + 1 < 8)
+                {
+                    if (board.GetPiece(new Square(piece.Row - 2, piece.Col + 1)) != null)
+                    {
+                        if (board.GetPiece(new Square(piece.Row - 2, piece.Col + 1)).Player != board.GetPiece(piece).Player)
+                        {
+                            moves = moves.Concat(new[] { new Square(piece.Row - 2, piece.Col + 1) });
+                        }
+                    }
+                    else
+                    {
+                        moves = moves.Concat(new[] { new Square(piece.Row - 2, piece.Col + 1) });
+                    }
+                }
 
-            if (piece.Row - 1 >= 0 && piece.Col + 2 < 8)
-            {
-                moves = moves.Concat(new[] { new Square(piece.Row - 1, piece.Col + 2) });
-            }
-
-            if (piece.Row - 1 >= 0 && piece.Col - 2 >= 0)
-            {
-                moves = moves.Concat(new[] { new Square(piece.Row - 1, piece.Col - 2) });
-            }
-
-            if (piece.Row - 2 >= 0 && piece.Col + 1 < 8)
-            {
-                moves = moves.Concat(new[] { new Square(piece.Row - 2, piece.Col + 1) });
-            }
-
-            if (piece.Row - 2 >= 0 && piece.Col - 1 >= 0)
-            {
-                moves = moves.Concat(new[] { new Square(piece.Row - 2, piece.Col - 1) });
+                if (piece.Col - 1 >= 0)
+                {
+                    if (board.GetPiece(new Square(piece.Row - 2, piece.Col - 1)) != null)
+                    {
+                        if (board.GetPiece(new Square(piece.Row - 2, piece.Col - 1)).Player != board.GetPiece(piece).Player)
+                        {
+                            moves = moves.Concat(new[] { new Square(piece.Row - 2, piece.Col - 1) });
+                        }
+                    }
+                    else
+                    {
+                        moves = moves.Concat(new[] { new Square(piece.Row - 2, piece.Col - 1) });
+                    }
+                }
             }
 
             return moves;
